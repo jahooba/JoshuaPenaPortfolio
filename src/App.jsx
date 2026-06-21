130d27df9b22b0cc9525be75979796d9163ba1c1
jahooba <josh.uapena20002@gmail.com>
2026-06-21 14:24:21 -0700

    Migrated to JoshuaPenaPortfolio directory

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
 import heroImg from './assets/background.jpg'
 import profileImg from './assets/profile2.jpg'
 import './App.css'
-import NavBar from './components/NavBar'
-import AboutMe from './components/AboutMe'
-import Projects from './components/Projects'
-import Contact from './components/Contact'
+import NavBar from '/src/components/NavBar'
+import AboutMe from '/src/components/AboutMe'
+import Projects from '/src/components/Projects'
+import Contact from '/src/components/Contact'
 
 function App() {
 
